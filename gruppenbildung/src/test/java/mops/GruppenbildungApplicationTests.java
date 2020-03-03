8fe72313f7699d9da23c8466a1e9064e82ba055a
nibrand <nils.brandenburg@hhu.de>
2020-03-03 11:28:03 +0100

    Move starting class to package mops

--- gruppenbildung/src/test/java/mops/GruppenbildungApplicationTests.java
+++ gruppenbildung/src/test/java/mops/GruppenbildungApplicationTests.java
@@ -1,4 +1,4 @@
-package mops.gruppen1;
+package mops;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GruppenbildungApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
