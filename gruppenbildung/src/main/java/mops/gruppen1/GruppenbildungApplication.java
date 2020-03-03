d6a038a854dd0c1242f72495ee3ef631875b9bc1
CarolinWin <carolin.winkels@gmx.de>
2020-03-03 10:25:58 +0100

    Initital Commit

---
+++ gruppenbildung/src/main/java/mops/gruppen1/GruppenbildungApplication.java
@@ -0,0 +1,13 @@
+package mops.gruppen1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruppenbildungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GruppenbildungApplication.class, args);
+	}
+
+}
---
+++ gruppenbildung/src/test/java/de/hhu/propra/gruppen1/gruppenbildung/GruppenbildungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhu.propra.gruppen1.gruppenbildung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruppenbildungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
