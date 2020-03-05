43b2a64a97099d8e15f08e03fae02a4037935b79
nibrand <nils.brandenburg@hhu.de>
2020-03-05 11:18:20 +0100

    Add relational annotations to Membership class

--- gruppenbildung/src/main/java/mops/gruppen1/domain/Membership.java
+++ gruppenbildung/src/main/java/mops/gruppen1/domain/Membership.java
@@ -1,6 +1,12 @@
 package mops.gruppen1.domain;
 
+import javax.persistence.ManyToOne;
+
 public class Membership {
+
+    @ManyToOne
     private User user;
+
+    @ManyToOne
     private Group group;
 }
