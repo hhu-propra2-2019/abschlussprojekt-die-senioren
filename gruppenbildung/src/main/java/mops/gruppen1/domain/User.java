3e7082d362ddf62b0e60e20c79b1841e63620cad
TImA97 <tim.angelike@gmx.de>
2020-03-05 10:16:47 +0100

    Add parameters and annotations to User class

--- gruppenbildung/src/main/java/mops/gruppen1/domain/User.java
+++ gruppenbildung/src/main/java/mops/gruppen1/domain/User.java
@@ -1,4 +1,15 @@
 package mops.gruppen1.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@EqualsAndHashCode
+@AllArgsConstructor
 public class User {
+    private UUID id;
+    private String name;
 }
