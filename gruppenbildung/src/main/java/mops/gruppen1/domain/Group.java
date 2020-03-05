8cc8ff2929194d45fa360f5563c2a25f60293e54
TImA97 <tim.angelike@gmx.de>
2020-03-05 11:08:59 +0100

    Add jpa dependency and annotate IDs of existing entities

--- gruppenbildung/src/main/java/mops/gruppen1/domain/Group.java
+++ gruppenbildung/src/main/java/mops/gruppen1/domain/Group.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.List;
 import java.util.UUID;
 
@@ -11,6 +13,8 @@ import java.util.UUID;
 @EqualsAndHashCode
 @AllArgsConstructor
 public class Group {
+    @Id
+    @GeneratedValue
     private UUID id;
     private GroupName name;
     private GroupDescription description;
--- gruppenbildung/src/main/java/mops/gruppen1/domain/User.java
+++ gruppenbildung/src/main/java/mops/gruppen1/domain/User.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.UUID;
 
 @Getter
 @EqualsAndHashCode
 @AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue
     private UUID id;
     private String name;
 }
