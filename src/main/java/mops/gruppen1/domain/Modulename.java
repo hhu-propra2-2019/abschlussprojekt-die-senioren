4376ed84041dd8534b501fd1199ac84af503faa9
Ritsch1 <r.plesz@yahoo.de>
2020-03-09 18:59:41 +0100

    Added Wrapper - Class Modulename.

---
+++ src/main/java/mops/gruppen1/domain/Modulename.java
@@ -0,0 +1,11 @@
+package mops.gruppen1.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Modulename {
+
+    private String modulename;
+}
