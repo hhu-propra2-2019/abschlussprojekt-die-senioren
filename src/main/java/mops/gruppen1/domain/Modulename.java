6d6f7380d85360e6281763cd12ae4f6364de5891
Ritsch1 <r.plesz@yahoo.de>
2020-03-09 19:05:27 +0100

    Added JavaDoc to ModuleName - Class.

--- src/main/java/mops/gruppen1/domain/Modulename.java
+++ src/main/java/mops/gruppen1/domain/Modulename.java
@@ -3,6 +3,9 @@ package mops.gruppen1.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * Representing the name of a module.
+ */
 @Getter
 @AllArgsConstructor
 public class Modulename {
