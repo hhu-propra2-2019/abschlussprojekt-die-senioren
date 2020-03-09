94b802ace98e7d845b2efb183f27682a6f85208e
Ritsch1 <r.plesz@yahoo.de>
2020-03-09 19:53:45 +0100

    Added JavaDoc to GruppenbildungApplication - class.

--- src/main/java/mops/GruppenbildungApplication.java
+++ src/main/java/mops/GruppenbildungApplication.java
@@ -3,11 +3,12 @@ package mops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Class that starts up the application.
+ */
 @SpringBootApplication
 public class GruppenbildungApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GruppenbildungApplication.class, args);
     }
-
 }
