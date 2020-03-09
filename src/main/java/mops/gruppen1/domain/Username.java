5e6a13de9ee9d3152b33ae57bd5d9147cc685b4f
Ritsch1 <r.plesz@yahoo.de>
2020-03-09 19:08:01 +0100

    Added Wrapper - class Username.

---
+++ src/main/java/mops/gruppen1/domain/Username.java
@@ -0,0 +1,14 @@
+package mops.gruppen1.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Representing the name of a user.
+ */
+@Getter
+@AllArgsConstructor
+public class Username {
+
+    private String username;
+}
