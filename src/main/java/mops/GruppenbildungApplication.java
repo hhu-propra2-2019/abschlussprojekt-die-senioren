d149c86b4b8628cd9318430e8d63eec1eef7ff1a
Ritsch1 <r.plesz@yahoo.de>
2020-03-09 19:56:34 +0100

    Added a JavaDoc to the main - method in GruppenbildungApplication - class.

--- src/main/java/mops/GruppenbildungApplication.java
+++ src/main/java/mops/GruppenbildungApplication.java
@@ -8,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class GruppenbildungApplication {
+
+    /**
+     * Method that starts up the application.
+     * @param args Arguments that can be handled to the app per console.
+     */
     public static void main(String[] args) {
         SpringApplication.run(GruppenbildungApplication.class, args);
     }
