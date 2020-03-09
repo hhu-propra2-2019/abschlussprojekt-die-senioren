bda89b6bdd229beecf0cbef608b7d94b4dc0a2fc
Ritsch1 <r.plesz@yahoo.de>
2020-03-09 19:04:36 +0100

    Added JavaDoc to ModuleSemester - Class.

--- src/main/java/mops/gruppen1/domain/ModuleSemester.java
+++ src/main/java/mops/gruppen1/domain/ModuleSemester.java
@@ -3,8 +3,13 @@ package mops.gruppen1.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+
 @Getter
 @AllArgsConstructor
+
+/**
+  Representing the semester of a module.
+ */
 public class ModuleSemester {
 
     private int moduleSemester;
