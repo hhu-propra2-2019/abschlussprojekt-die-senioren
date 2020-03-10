c57e115ecf77d122e74abc0dc31525de1a179ce5
TImA97 <tim.angelike@gmx.de>
2020-03-10 09:10:10 +0100

    Add and implement groupServic

--- src/main/java/mops/gruppen1/applicationService/GroupService.java
+++ src/main/java/mops/gruppen1/applicationService/GroupService.java
@@ -1,4 +1,23 @@
 package mops.gruppen1.applicationService;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import mops.gruppen1.domain.Group;
+import mops.gruppen1.domain.Membership;
+import mops.gruppen1.domain.User;
+import mops.gruppen1.domain.Username;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+@Getter
+@EqualsAndHashCode
+@AllArgsConstructor
 public class GroupService {
+    HashMap<Group, List<Membership>> groupToMembers;
+    HashMap<User, List<Membership>> userToMembers;
+    HashSet<User> users;
+    HashSet<Group> groups;
 }
