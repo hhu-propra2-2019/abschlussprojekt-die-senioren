15d47889c37b00fd3f205ba711222322f7cb9c55
stelask <stela.skandro@hhu.de>
2020-03-10 11:03:34 +0100

    Enable RequestMapping on "/gruppen1"

--- src/main/java/mops/gruppen1/Controller/GroupController.java
+++ src/main/java/mops/gruppen1/Controller/GroupController.java
@@ -2,10 +2,10 @@ package mops.gruppen1.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-// Leave commented until stylguide is fixed for this route
-//@RequestMapping("/gruppen1")
+@RequestMapping("/gruppen1")
 public class GroupController {
 
     @GetMapping("/")
