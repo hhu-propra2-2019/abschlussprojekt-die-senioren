909c91d55088f21fce08f812cd22aa3b42f655de
nibrand <nils.brandenburg@hhu.de>
2020-03-11 11:49:48 +0100

    Add incomplete test for the transform method in EventService

--- src/test/java/mops/EventServiceTests.java
+++ src/test/java/mops/EventServiceTests.java
@@ -1,9 +1,13 @@
 package mops;
 
+import mops.gruppen1.applicationService.EventService;
+import mops.gruppen1.data.EventDTO;
 import mops.gruppen1.data.EventRepo;
+import mops.gruppen1.domain.events.Event;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.mockito.Mockito.mock;
@@ -12,16 +16,25 @@ import static org.mockito.Mockito.mock;
 @SpringBootTest(classes = GruppenbildungApplicationTests.class)
 class EventServiceTests {
 
-   // EventRepo eventRepoMock = mock(EventRepo.class);
+    static EventService eventService;
 
-//    @BeforeAll
-//    void InitEventRepoMock(){
-//        //eventRepoMock
-//    }
-//
-//    @Test
-//    void DTOIsNotNULL() {
-//
-//    }
+    @BeforeAll
+    static void InitEventRepoMock() {
+        eventService = new EventService();
+    }
+
+    @Test
+    void testTransform() {
+        String testUserName = "test_user";
+        String testGroupName = "test_group";
+        String testEventType = "GroupCreationEvent";
+        String testPayload = "test_payload";
+
+        EventDTO testEventDTO = new EventDTO(testUserName, testGroupName, testEventType, testPayload);
+
+        Event testEvent = eventService.transform(testEventDTO);
+
+
+    }
 
 }
