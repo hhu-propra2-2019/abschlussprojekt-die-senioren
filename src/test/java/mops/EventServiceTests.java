d8d27b5eefd8136ffe7bb258f77df182225d55f6
T-Wiegand <towie106@uni-duesseldorf.de>
2020-03-11 09:18:10 +0100

    TestConfiguration of H2 database

---
+++ src/test/java/mops/EventServiceTests.java
@@ -0,0 +1,27 @@
+package mops;
+
+import mops.gruppen1.data.EventRepo;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.Mockito.mock;
+
+
+@SpringBootTest(classes = GruppenbildungApplicationTests.class)
+class EventServiceTests {
+
+   // EventRepo eventRepoMock = mock(EventRepo.class);
+
+//    @BeforeAll
+//    void InitEventRepoMock(){
+//        //eventRepoMock
+//    }
+//
+//    @Test
+//    void DTOIsNotNULL() {
+//
+//    }
+
+}
--- src/test/java/mops/GruppenbildungApplicationTests.java
+++ src/test/java/mops/GruppenbildungApplicationTests.java
@@ -3,7 +3,7 @@ package mops;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GruppenbildungApplicationTests.class)
 class GruppenbildungApplicationTests {
 
     @Test
