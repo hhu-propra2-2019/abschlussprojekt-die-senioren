7b7fd4fc680f111e603518e9258de94759bd295a
TImA97 <tim.angelike@gmx.de>
2020-03-12 09:24:11 +0100

    Modify testcontroller to mock keycloak authentication

--- src/test/java/mops/gruppen1/Controller/GroupControllerTest.java
+++ src/test/java/mops/gruppen1/Controller/GroupControllerTest.java
@@ -1,27 +1,64 @@
 package mops.gruppen1.Controller;
 
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.adapters.RefreshableKeycloakSecurityContext;
+import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+/**
+ * Test for Controller
+ * - Might have to be updated after Keycloak update
+ */
 @AutoConfigureMockMvc
-@WebMvcTest
+@SpringBootTest
 class GroupControllerTest {
     @Autowired
     MockMvc mvc;
 
+    @Autowired
+    WebApplicationContext context;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @Test
-    @WithMockUser(username = "root", password = "1234", roles = "ADMIN")
     void testIndex() throws Exception {
-        mvc.perform(get("/gruppen1/")).andExpect(status().isOk());
+        Set<String> roles = new HashSet<String>();
+        roles.add("studentin");
+        KeycloakAuthenticationToken token = new KeycloakAuthenticationToken(
+                new SimpleKeycloakAccount(
+                        Mockito.mock(KeycloakPrincipal.class, Mockito.RETURNS_DEEP_STUBS),
+                        roles,
+                        Mockito.mock(RefreshableKeycloakSecurityContext.class)),
+                true);
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(token);
+        mvc.perform(get("/gruppen1/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 }
