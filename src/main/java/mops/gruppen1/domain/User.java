ea0a59ff2ef22c10fbbc43ea55e56f98d1e42882
stelask <stela.skandro@hhu.de>
2020-03-13 11:59:56 +0100

    Add comments (renaming of attributes) in class User

--- src/main/java/mops/gruppen1/domain/User.java
+++ src/main/java/mops/gruppen1/domain/User.java
@@ -6,6 +6,6 @@ import java.util.UUID;
  * Representation of a user within the domain.
  */
 public class User {
-    private UUID userId;
-    private Username username;
+    private UUID userId; // umbenennen in Login name?
+    private Username username; // umbenennen in Klarnamen?
 }
