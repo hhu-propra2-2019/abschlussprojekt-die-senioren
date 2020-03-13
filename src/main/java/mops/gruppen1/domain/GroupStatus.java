c6131e7e9f87481ea084e2cc977e4a5a0c0e48f3
stelask <stela.skandro@hhu.de>
2020-03-13 11:59:09 +0100

    Add information for status of class GroupStatus only possibilities are 'active' or 'deactived'

--- src/main/java/mops/gruppen1/domain/GroupStatus.java
+++ src/main/java/mops/gruppen1/domain/GroupStatus.java
@@ -1,7 +1,7 @@
 package mops.gruppen1.domain;
 
 /**
- * Representation of a group - status.
+ * Representation of a group - status: is 'active' or 'deactived'
  */
 public class GroupStatus {
     private String status;
