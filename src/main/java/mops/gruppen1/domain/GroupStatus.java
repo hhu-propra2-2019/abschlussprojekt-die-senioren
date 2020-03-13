0932ec578de5e4aae30135cc084b65fa5049e579
l.hilger <Lena.Hilger@hhu.de>
2020-03-13 10:31:11 +0100

    Adjust javaDoc description for GroupStatus

--- src/main/java/mops/gruppen1/domain/GroupStatus.java
+++ src/main/java/mops/gruppen1/domain/GroupStatus.java
@@ -1,7 +1,7 @@
 package mops.gruppen1.domain;
 
 /**
- * Representation of a group - status.
+ * Representation of a group - status (active or deactivated).
  */
 public class GroupStatus {
     private String status;
