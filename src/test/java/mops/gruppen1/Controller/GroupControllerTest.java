3c86eb7854e9cbb6ab9903ada8a10c920545d39c
Ritsch1 <r.plesz@yahoo.de>
2020-03-13 10:19:36 +0100

    Created controller - test for groupcreation - page.

--- src/test/java/mops/gruppen1/Controller/GroupControllerTest.java
+++ src/test/java/mops/gruppen1/Controller/GroupControllerTest.java
@@ -95,4 +95,21 @@ class GroupControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("gruppenViewer"));
     }
+
+    @Test
+    void testGroupCreation() throws Exception {
+        Set<String> roles = new HashSet<String>();
+        roles.add("studentin");
+        KeycloakAuthenticationToken token = new KeycloakAuthenticationToken(
+                new SimpleKeycloakAccount(
+                        Mockito.mock(KeycloakPrincipal.class, Mockito.RETURNS_DEEP_STUBS),
+                        roles,
+                        Mockito.mock(RefreshableKeycloakSecurityContext.class)),
+                true);
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(token);
+        mvc.perform(get("/gruppen1/erstellen"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("erstellen"));
+    }
 }
