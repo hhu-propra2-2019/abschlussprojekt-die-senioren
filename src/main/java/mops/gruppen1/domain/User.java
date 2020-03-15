c7dbc09683e6c9cf60887e268aef4609a1d312a1
stelask <stela.skandro@hhu.de>
2020-03-15 21:43:30 +0100

    Change User representation has only username

--- src/main/java/mops/gruppen1/domain/User.java
+++ src/main/java/mops/gruppen1/domain/User.java
@@ -6,6 +6,5 @@ import java.util.UUID;
  * Representation of a user within the domain.
  */
 public class User {
-    private UUID userId; // umbenennen in Login name?
-    private Username username; // umbenennen in Klarnamen?
+    private Username username;
 }
