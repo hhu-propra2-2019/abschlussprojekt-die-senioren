47c9372b991a4c77b4e2fc45d9bd263803f58aa0
TImA97 <tim.angelike@gmx.de>
2020-03-16 11:00:31 +0100

    Add updateUserMemberships function to GroupDeletionEvent
    co-authored by: nibrand nils.brandenburg@hhu.de

--- src/main/java/mops/gruppen1/domain/events/GroupDeletionEvent.java
+++ src/main/java/mops/gruppen1/domain/events/GroupDeletionEvent.java
@@ -1,6 +1,9 @@
 package mops.gruppen1.domain.events;
 
-import mops.gruppen1.applicationService.GroupService;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import mops.gruppen1.domain.Group;
 import mops.gruppen1.domain.Membership;
 import mops.gruppen1.domain.User;
@@ -8,12 +11,36 @@ import mops.gruppen1.domain.User;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+@Getter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class GroupDeletionEvent implements Event {
 
+    Group group;
+    String deletedByUser;
 
     @Override
     public void execute(HashMap<Group, List<Membership>> groupToMembers, HashMap<User, List<Membership>> userToMembers, HashSet<User> users, HashSet<Group> groups) {
+        groupToMembers.remove(group);
+        groups.remove(group);
+        updateUserMemberships(userToMembers);
 
+    }
+
+    /**
+     * method deletes memberships taht map to deleted groups from userToMember Hash-Map
+     *
+     * @param userToMembers
+     */
+    private void updateUserMemberships(HashMap<User, List<Membership>> userToMembers) {
+        for (Map.Entry<User, List<Membership>> entry : userToMembers.entrySet()) {
+            List<Membership> memberships = entry.getValue();
+            List<Membership> newMemberships = memberships.stream().filter(member -> !member.getGroup().equals(group)).collect(Collectors.toList());
+            entry.setValue(newMemberships);
+        }
     }
 }
