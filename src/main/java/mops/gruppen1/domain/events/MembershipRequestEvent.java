ee7eca17fe0835cd04820ba0073fb7a90cfe44e7
stelask <stela.skandro@hhu.de>
2020-03-17 11:37:40 +0100

    Implement attributes and method execute in MembershipRequestEvent Co-authored-by: M4XIMUS1 <maxreinhard148@web.de>

--- src/main/java/mops/gruppen1/domain/events/MembershipRequestEvent.java
+++ src/main/java/mops/gruppen1/domain/events/MembershipRequestEvent.java
@@ -1,8 +1,6 @@
 package mops.gruppen1.domain.events;
 
-import mops.gruppen1.domain.Group;
-import mops.gruppen1.domain.Membership;
-import mops.gruppen1.domain.User;
+import mops.gruppen1.domain.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -14,9 +12,19 @@ import java.util.List;
  */
 public class MembershipRequestEvent implements IEvent {
 
+    String groupId;
+    String userName;
+    String membershipType;
 
     @Override
     public void execute(HashMap<Group, List<Membership>> groupToMembers, HashMap<User, List<Membership>> userToMembers, HashMap<String, User> users, HashMap<String, Group> groups) {
+        Group group = groups.get(groupId);
+        User user = users.get(userName);
+        Type membershipType = Type.valueOf(this.membershipType);
+
+        Membership membership = new Membership(user,group, membershipType, Status.PENDING);
+        groupToMembers.get(group).add(membership);
+        userToMembers.get(user).add(membership);
 
     }
 }
