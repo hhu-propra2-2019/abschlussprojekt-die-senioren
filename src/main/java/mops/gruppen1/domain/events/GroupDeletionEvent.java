56edf04f7375c0e0964545003d57cb69cbbd2603
l-hilger <49523327+l-hilger@users.noreply.github.com>
2020-03-17 10:47:20 +0100

    Update GroupDeletionEvent (insert lines)

--- src/main/java/mops/gruppen1/domain/events/GroupDeletionEvent.java
+++ src/main/java/mops/gruppen1/domain/events/GroupDeletionEvent.java
@@ -1,14 +1,16 @@
 package mops.gruppen1.domain.events;
 
-import mops.gruppen1.applicationService.GroupService;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import mops.gruppen1.domain.Group;
 import mops.gruppen1.domain.Membership;
 import mops.gruppen1.domain.User;
-
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Getter
 @EqualsAndHashCode
@@ -19,9 +21,16 @@ import java.util.List;
  */
 public class GroupDeletionEvent implements Event {
 
+    String groupId;
+    String deletedByUser;
 
     @Override
     public void execute(HashMap<Group, List<Membership>> groupToMembers, HashMap<User, List<Membership>> userToMembers, HashMap<String, User> users, HashMap<String, Group> groups) {
+        Group group = groups.get(groupId);
+        groupToMembers.remove(group);
+        groups.remove(groupId);
+        updateUserMemberships(userToMembers, groups);
+    }
 
     /**
      * method deletes memberships that map to deleted groups from userToMember Hash-Map
