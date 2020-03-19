90f17c275f933db176a5e600a623d8f1eedcb07e
Ritsch1 <r.plesz@yahoo.de>
2020-03-19 09:50:30 +0100

    Implemented method for finding the updated member in a group.
    Co-authored-by: CarolinWin <carolin.winkels@gmx.de>

--- src/main/java/mops/gruppen1/domain/events/MembershipUpdateEvent.java
+++ src/main/java/mops/gruppen1/domain/events/MembershipUpdateEvent.java
@@ -12,9 +12,29 @@ import java.util.List;
  */
 public class MembershipUpdateEvent implements IEvent {
 
+    private String groupId;
+    private String memberId;
+    private String updatedBy;
+    private String updatedTo;
 
     @Override
     public void execute(HashMap<String, List<Membership>> groupToMembers, HashMap<String, List<Membership>> userToMembers, HashMap<String, User> users, HashMap<String, Group> groups) {
 
+    }
+
+    /**
+     * Finds the member that is to be updated in a group.
+     *
+     * @param groups The group in which a member is searched for.
+     * @return The member that matches the memberId.
+     */
+    private Membership findUpdatedMember(HashMap<String, Group> groups) {
+        Group group = groups.get(groupId);
+        Membership membership = group.getMembers().stream()
+                .filter(member -> memberId.equals(member.getMemberid().toString()))
+                .findFirst()
+                .orElse(null);
+
+        return membership;
     }
 }
