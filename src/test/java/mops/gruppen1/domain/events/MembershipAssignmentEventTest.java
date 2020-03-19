b15d71626bfec981ab100dba611489e66bcc2981
M4XIMUS1 <MaxReinhard148@web.de>
2020-03-19 09:06:30 +0100

    Adjust MembershipAssignmentEventTest.
    Co-authored-by: stelask <stela.skandro@hhu.de>

---
+++ src/test/java/mops/gruppen1/domain/events/MembershipAssignmentEventTest.java
@@ -0,0 +1,41 @@
+package mops.gruppen1.domain.events;
+
+import mops.gruppen1.domain.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class MembershipAssignmentEventTest {
+
+    private TestSetup testSetup;
+
+    @BeforeEach
+    void setup() {
+        this.testSetup = new TestSetup();
+    }
+
+    @Tag("EventTest")
+    @DisplayName("Test MembershipAssignmentEvent")
+    @Test
+    void execute() {
+        //arrange
+        Group groupOne = testSetup.groupOne;
+        String groupId = testSetup.groupOne.getGroupId().toString();
+        Username username = new Username("Hand-Test");
+        User newUser = new User(username);
+        testSetup.users.put(username.toString(), newUser);
+        Type membershipType = Type.VIEWER;
+
+        MembershipAssignmentEvent membershipAssignmentEvent = new MembershipAssignmentEvent(groupId,username.toString(),membershipType.toString());
+
+        //act
+        membershipAssignmentEvent.execute(testSetup.groupToMembers, testSetup.userToMembers, testSetup.users, testSetup.groups);
+
+        //assert
+        assertThat(groupOne.getMembers().stream().filter(membership -> membership.getUser().equals(newUser)));
+    }
+}
