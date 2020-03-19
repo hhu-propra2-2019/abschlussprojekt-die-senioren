1d52bdf138c0e7324473cfb791d02e17c0aab182
M4XIMUS1 <MaxReinhard148@web.de>
2020-03-19 09:48:41 +0100

    Implement MaterialDeletionEvent.
    Co-authored-by: stelask <stela.skandro@hhu.de>

--- src/main/java/mops/gruppen1/domain/events/MaterialDeletionEvent.java
+++ src/main/java/mops/gruppen1/domain/events/MaterialDeletionEvent.java
@@ -1,5 +1,7 @@
 package mops.gruppen1.domain.events;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import mops.gruppen1.domain.Group;
 import mops.gruppen1.domain.Membership;
 import mops.gruppen1.domain.User;
@@ -11,11 +13,14 @@ import java.util.List;
  * Delete link of the external MaterialService in the Group
  * or change representation
  */
+@Getter
+@AllArgsConstructor
 public class MaterialDeletionEvent implements IEvent {
-
+    private String groupId;
 
     @Override
     public void execute(HashMap<String, List<Membership>> groupToMembers, HashMap<String, List<Membership>> userToMembers, HashMap<String, User> users, HashMap<String, Group> groups) {
-
+        Group group = groups.get(groupId);
+        group.setMaterial(null);
     }
 }
