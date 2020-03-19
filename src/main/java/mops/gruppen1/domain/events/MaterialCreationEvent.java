74064efb56100828f06030911c0ec6d48efb0a25
stelask <stela.skandro@hhu.de>
2020-03-19 09:37:40 +0100

    Implement ClassMaterialCreationEvent
    Co-authored-by: M4XIMUS1 <maxreinhard148@web.de>

--- src/main/java/mops/gruppen1/domain/events/MaterialCreationEvent.java
+++ src/main/java/mops/gruppen1/domain/events/MaterialCreationEvent.java
@@ -1,20 +1,26 @@
 package mops.gruppen1.domain.events;
 
-import mops.gruppen1.domain.Group;
-import mops.gruppen1.domain.Membership;
-import mops.gruppen1.domain.User;
+import lombok.AllArgsConstructor;
+import mops.gruppen1.domain.*;
 
 import java.util.HashMap;
 import java.util.List;
 
+
 /**
  * Add a link of the external MaterialService to the Group
+ * @all: TODO: Add check for already existing Material link to Group Service
  */
+@AllArgsConstructor
 public class MaterialCreationEvent implements IEvent {
-
+    private String groupId;
+    private String materialLink;
+    private String createdBy;
 
     @Override
     public void execute(HashMap<String, List<Membership>> groupToMembers, HashMap<String, List<Membership>> userToMembers, HashMap<String, User> users, HashMap<String, Group> groups) {
-
+        Group group = groups.get(groupId);
+        Material material = new Material(materialLink);
+     //   group.setAppointment(appointment);
     }
 }
