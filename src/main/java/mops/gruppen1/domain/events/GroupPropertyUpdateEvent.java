1be87e4cbbbcade1ba7a597f9a923bfa7f4f4e6f
Nils Brandenburg <47447214+nibrand@users.noreply.github.com>
2020-03-19 11:27:31 +0100

    Update GroupPropertyUpdateEvent.java

--- src/main/java/mops/gruppen1/domain/events/GroupPropertyUpdateEvent.java
+++ src/main/java/mops/gruppen1/domain/events/GroupPropertyUpdateEvent.java
@@ -18,14 +18,14 @@ public class GroupPropertyUpdateEvent implements IEvent {
 
     private String groupId;
     private String updatedBy;
-    private String name;
+    private String groupName;
     private String description;
     private String groupType;
 
     @Override
     public void execute(HashMap<String, List<Membership>> groupToMembers, HashMap<String, List<Membership>> userToMembers, HashMap<String, User> users, HashMap<String, Group> groups) {
         Group group = groups.get(groupId);
-        group.setName(new GroupName(this.name));
+        group.setName(new GroupName(this.groupName));
         group.setDescription(new GroupDescription(this.description));
         group.setGroupType((GroupType.valueOf(this.groupType)));
     }
