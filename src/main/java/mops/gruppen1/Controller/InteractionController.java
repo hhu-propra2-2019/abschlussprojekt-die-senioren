ff4a2ef5d8b8b36793630d196725528937409c69
Rotfuechsin <johil101@hhu.de>
2020-03-20 11:18:52 +0100

    Integrate Swagger
    Co-authored-by: l-hilger <lena.hilger@hhu.de>
    Co-authored-by: T-Wiegand <towie106@uni-duesseldorf.de>

---
+++ src/main/java/mops/gruppen1/Controller/InteractionController.java
@@ -0,0 +1,30 @@
+package mops.gruppen1.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gruppen1")
+public class InteractionController {
+
+    @GetMapping("/userInGroup")
+    public String userInGroup(@RequestParam(value = "name", defaultValue = "nope") String name) {
+        // if default-value => request is not valid, username must be given
+        //Check if user is in Group
+        //return yes/no?
+        return name;
+    }
+
+    @GetMapping("/isAdmin")
+    public String isAdmin(@RequestParam(value = "name", defaultValue = "noId") String name,
+                          @RequestParam(value = "group", defaultValue = "noGroupID") String group) {
+        // if default-value => request is not valid, username must be given
+        // check if user ist admin in Group
+        //return yes/no?
+        return name;
+    }
+
+}
