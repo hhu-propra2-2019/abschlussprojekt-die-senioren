5933434e034d815358583f7b999360ab11740af8
TImA97 <tim.angelike@gmx.de>
2020-03-20 09:14:47 +0100

    add check methods to GroupService
    co-authored by: nibrand nils.brandenburg@hhu.de

--- src/main/java/mops/gruppen1/applicationService/GroupService.java
+++ src/main/java/mops/gruppen1/applicationService/GroupService.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import mops.gruppen1.data.EventDTO;
-import mops.gruppen1.domain.Group;
-import mops.gruppen1.domain.Membership;
-import mops.gruppen1.domain.User;
+import mops.gruppen1.domain.*;
 import mops.gruppen1.domain.events.GroupCreationEvent;
 import mops.gruppen1.domain.events.GroupDeletionEvent;
 import mops.gruppen1.domain.events.IEvent;
@@ -43,6 +41,74 @@ public class GroupService {
                 users,
                 groups
         ));
+    }
+
+    private boolean isGroupActive(String groupId) {
+        Group group = groups.get(groupId);
+        boolean isActive = group.getGroupStatus().equals(GroupStatus.ACTIVE);
+        return isActive;
+    }
+
+    private boolean isPublic(String groupId) {
+        Group group = groups.get(groupId);
+        boolean isPublic = group.getGroupType().equals(GroupType.PUBLIC);
+        return isPublic;
+    }
+
+    private boolean isRestricted(String groupId) {
+        Group group = groups.get(groupId);
+        boolean isRestricted = group.getGroupType().equals(GroupType.RESTRICTED);
+        return isRestricted;
+    }
+
+    private boolean isAdmin(String userName, String groupId) {
+        User user = users.get(userName);
+        List<Membership> memberships = userToMembers.get(userName);
+        Group group = groups.get(groupId);
+        Membership membership = getMembership(memberships, group);
+
+        boolean isAdmin = membership.getType().equals(Type.ADMIN);
+        return isAdmin;
+    }
+
+    private boolean isActive(String userName, String groupId) {
+        User user = users.get(userName);
+        List<Membership> memberships = userToMembers.get(userName);
+        Group group = groups.get(groupId);
+        Membership membership = getMembership(memberships, group);
+
+        boolean isActive = membership.getStatus().equals(Status.ACTIVE);
+        return isActive;
+    }
+
+    private boolean isPending(String userName, String groupId) {
+        User user = users.get(userName);
+        List<Membership> memberships = userToMembers.get(userName);
+        Group group = groups.get(groupId);
+        Membership membership = getMembership(memberships, group);
+
+        boolean isPending = membership.getStatus().equals(Status.PENDING);
+        return isPending;
+    }
+
+    private boolean isMember(String userName, String groupId) {
+        User user = users.get(userName);
+        List<Membership> memberships = userToMembers.get(userName);
+        Group group = groups.get(groupId);
+
+        boolean isMember = getMembership(memberships, group) != null;
+        return isMember;
+    }
+    
+    private Membership getMembership(List<Membership> memberships, Group group) {
+        Membership membership = null;
+        for (Membership m : memberships) {
+            if (m.getGroup().equals(group)) {
+                membership = m;
+                break;
+            }
+        }
+        return membership;
     }
 
     public void createGroup(String groupDescription, String groupName, String groupCourse, String groupCreator, String groupType) {
