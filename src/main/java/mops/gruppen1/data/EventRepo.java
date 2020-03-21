4936bf72e6ac102deb90c25786917f7b28a8b0f9
M4XIMUS1 <MaxReinhard148@web.de>
2020-03-21 16:06:07 +0100

    Adjust EventRepo.

--- src/main/java/mops/gruppen1/data/EventRepo.java
+++ src/main/java/mops/gruppen1/data/EventRepo.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
  * Repository to fetch event data from database
  */
 @Repository
-public interface EventRepo extends CrudRepository<EventDTO, Long>{
+public interface EventRepo extends CrudRepository<EventDTO, Long> {
 
 }
