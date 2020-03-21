738ae8f306af6dac56e5d0934cf018534f0dce58
stelask <stela.skandro@hhu.de>
2020-03-21 11:16:38 +0100

    Adjust Javadoc for MembershipRejectionEvent

--- src/main/java/mops/gruppen1/domain/events/MembershipRejectionEvent.java
+++ src/main/java/mops/gruppen1/domain/events/MembershipRejectionEvent.java
@@ -12,7 +12,8 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * change status of Membership to 'REJECTED'
+ * For restricted groups and a PENDING Membership
+ * Change status of Membership to 'REJECTED'
  */
 @Getter
 @EqualsAndHashCode
@@ -23,6 +24,14 @@ public class MembershipRejectionEvent implements IEvent {
     private String groupId;
     private String userName;
 
+    /**
+     * Sets the membership of the user to REJECTED in all datastructures
+     *
+     * @param groupToMembers Hashmap that maps a String(groupId) to a list of memberships.
+     * @param userToMembers  Hashmap that maps a String(userId) to a list of memberships
+     * @param users          Hashmap that maps a String(userId) to a user.
+     * @param groups         Hashmap that maps a String(groupId) to memberships within the group.
+     */
     @Override
     public void execute(HashMap<String, List<Membership>> groupToMembers, HashMap<String, List<Membership>> userToMembers, HashMap<String, User> users, HashMap<String, Group> groups) {
         User user = users.get(userName);
@@ -33,6 +42,12 @@ public class MembershipRejectionEvent implements IEvent {
         membership.setMembershipStatus(MembershipStatus.REJECTED);
     }
 
+    /**
+     * finds the membership of the user in the group to which access in refused
+     * @param memberships The user's memberships
+     * @param groupId The group the user is not accepted to
+     * @return the membership belonging to groupID and contained in memberships
+     */
     private Membership getMembership(List<Membership> memberships, String groupId) {
         Membership membership = null;
         for (Membership m : memberships) {
