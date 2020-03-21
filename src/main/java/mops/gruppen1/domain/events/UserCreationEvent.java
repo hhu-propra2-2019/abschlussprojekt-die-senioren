5628c0bd63b214e9caaf8aedeb467b50d3f70f00
stelask <stela.skandro@hhu.de>
2020-03-21 11:24:53 +0100

    Adjust Javadoc for UserCreationEvent

--- src/main/java/mops/gruppen1/domain/events/UserCreationEvent.java
+++ src/main/java/mops/gruppen1/domain/events/UserCreationEvent.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 /**
  * create new instance of class User
- * Add to HashMap users
+ * Add to users and userToMembers
  */
 @Getter
 @AllArgsConstructor
@@ -22,6 +22,14 @@ import java.util.List;
 public class UserCreationEvent implements IEvent {
     private String username;
 
+    /**
+     * Creates a new User, is added to Hashmap users and Hashmap userToMembers with empty list of memberships
+     *
+     * @param groupToMembers Hashmap that maps a String(groupId) to a list of memberships.
+     * @param userToMembers  Hashmap that maps a String(userId) to a list of memberships
+     * @param users          Hashmap that maps a String(userId) to a user.
+     * @param groups         Hashmap that maps a String(groupId) to memberships within the group.
+     */
     @Override
     public void execute(HashMap<String, List<Membership>> groupToMembers, HashMap<String, List<Membership>> userToMembers, HashMap<String, User> users, HashMap<String, Group> groups) {
         Username username = new Username(this.username);
