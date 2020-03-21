3d45f6364452a47aba39c2e51d991ebc11d8449e
stelask <stela.skandro@hhu.de>
2020-03-21 11:19:28 +0100

    Adjust Javadoc for MembershipRequestEvent

--- src/main/java/mops/gruppen1/domain/events/MembershipRequestEvent.java
+++ src/main/java/mops/gruppen1/domain/events/MembershipRequestEvent.java
@@ -11,7 +11,7 @@ import java.util.List;
 /**
  * For restricted Groups:
  * Create a Membership with Status 'PENDING'
- * Add Membership to groupToMembers and userToMembers
+ * Add Membership to groupToMembers, userToMembers and group's list of members
  */
 @Getter
 @AllArgsConstructor
@@ -22,6 +22,14 @@ public class MembershipRequestEvent implements IEvent {
     private String userName;
     private String membershipType;
 
+    /**
+     * Adds a new membership with Status PENDING in all datastructures
+     *
+     * @param groupToMembers Hashmap that maps a String(groupId) to a list of memberships.
+     * @param userToMembers  Hashmap that maps a String(userId) to a list of memberships
+     * @param users          Hashmap that maps a String(userId) to a user.
+     * @param groups         Hashmap that maps a String(groupId) to memberships within the group.
+     */
     @Override
     public void execute(HashMap<String, List<Membership>> groupToMembers, HashMap<String, List<Membership>> userToMembers, HashMap<String, User> users, HashMap<String, Group> groups) {
         Group group = groups.get(groupId);
