577af706c84de9a41b9130bd253f9499e5238eb6
nibrand <nils.brandenburg@hhu.de>
2020-03-23 12:38:17 +0100

    Add ApplicationService class

---
+++ src/main/java/mops/gruppen1/applicationService/ApplicationService.java
@@ -0,0 +1,8 @@
+package mops.gruppen1.applicationService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ApplicationService {
+    @Autowired
+    GroupService groupService;
+}
