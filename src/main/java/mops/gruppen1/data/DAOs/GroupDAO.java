61d508ef6ef99c34a11bdedccaf39671522cac28
l.hilger <Lena.Hilger@hhu.de>
2020-03-24 11:06:48 +0100

    implement groupDAO
    Co-authored-by: T-Wiegand <towie106@uni-duesseldorf.de>

--- src/main/java/mops/gruppen1/data/DAOs/GroupDAO.java
+++ src/main/java/mops/gruppen1/data/DAOs/GroupDAO.java
@@ -1,4 +1,19 @@
 package mops.gruppen1.data.DAOs;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class GroupDAO {
+    private String groupId;
+    private String groupName;
+    private String course;
+    private String groupDescritption;
+
+    public GroupDAO(String groupId, String groupName, String groupDescritption) {
+        this.groupId = groupId;
+        this.groupName = groupName;
+        this.groupDescritption = groupDescritption;
+    }
+
+
 }
