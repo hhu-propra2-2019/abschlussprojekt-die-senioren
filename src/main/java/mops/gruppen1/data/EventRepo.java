88ec33c38c830249f2aa6e4caa636bf248a56d9a
stelask <stela.skandro@hhu.de>
2020-03-24 11:12:37 +0100

    Implement EventDTO Projections and add EventRepo methods
    Co-authored-by: M4XIMUS1 <maxreinhard148@web.de>

--- src/main/java/mops/gruppen1/data/EventRepo.java
+++ src/main/java/mops/gruppen1/data/EventRepo.java
@@ -3,10 +3,14 @@ package mops.gruppen1.data;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 /**
  * Repository to fetch event data from database
  */
 @Repository
 public interface EventRepo extends CrudRepository<EventDTO, Long> {
+    Collection<EventIdOnly> findFirstByOrderByEventIdDesc();
+    Collection<GroupIdOnly> findByIdGreaterThan(Long id);
 
 }
