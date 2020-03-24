95b6eccfd00d3c8449fa8d380615ecc38d3bee6f
stelask <stela.skandro@hhu.de>
2020-03-24 11:40:17 +0100

    Change EventRepo to PagingAndSortingRepository and fix issues
    Co-authored-by: M4XIMUS1 <maxreinhard148@web.de>

--- src/main/java/mops/gruppen1/data/EventRepo.java
+++ src/main/java/mops/gruppen1/data/EventRepo.java
@@ -1,16 +1,17 @@
 package mops.gruppen1.data;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
+import java.util.List;
 
 /**
  * Repository to fetch event data from database
  */
 @Repository
-public interface EventRepo extends CrudRepository<EventDTO, Long> {
-    Collection<EventIdOnly> findFirstByOrderByEventIdDesc();
-    Collection<GroupIdOnly> findByIdGreaterThan(Long id);
+public interface EventRepo extends PagingAndSortingRepository<EventDTO, Long> {
+    List<EventIdOnly> findTopByOrderByIdDesc();
+
+    List<GroupIdOnly> findAllByIdAfter(Long id);
 
 }
