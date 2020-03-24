613e1f641ab4ec2c2c5c6270c511c1e9e84d0a42
TImA97 <tim.angelike@gmx.de>
2020-03-24 11:49:53 +0100

    Add event methods to ApplicationService
    co-authored by: nibrand nils.brandenburg@hhu.de

--- src/main/java/mops/gruppen1/applicationService/ApplicationService.java
+++ src/main/java/mops/gruppen1/applicationService/ApplicationService.java
@@ -1,16 +1,15 @@
 package mops.gruppen1.applicationService;
 
-import mops.gruppen1.domain.Group;
-import mops.gruppen1.domain.Membership;
-import mops.gruppen1.domain.MembershipStatus;
-import mops.gruppen1.domain.User;
+import mops.gruppen1.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class ApplicationService {
     @Autowired
     GroupService groupService;
@@ -66,6 +65,75 @@ public class ApplicationService {
             }
         }
         return requestedUsers;
+    }
+
+    public ValidationResult createGroup(String groupDescription, String groupName, String groupCourse,
+                                        String groupCreator, String groupType, List<String> users) {
+        List<ValidationResult> validationResults = new ArrayList<>();
+        validationResults
+                .add(groupService.createGroup(groupDescription, groupName, groupCourse, groupCreator, groupType));
+        validationResults
+                .add(groupService.assignMembership(groupCreator, groupService.getLastCreatedGroup(), "ADMIN"));
+        for (String user : users) {
+            validationResults
+                    .add(groupService.assignMembership(user, groupService.getLastCreatedGroup(), "VIEWER"));
+        }
+        ValidationResult validationResult = groupService.collectCheck(validationResults);
+        return validationResult;
+    }
+
+    public ValidationResult deleteGroup(String groupId, String userName) {
+        ValidationResult validationResult = groupService.deleteGroup(groupId, userName);
+        return validationResult;
+    }
+
+    public ValidationResult updateGroupProperties(String groupId, String updatedBy, String groupName,
+                                                  String description, String groupType) {
+        ValidationResult validationResult =
+                groupService.updateGroupProperties(groupId, updatedBy, groupName, description, groupType);
+        return validationResult;
+    }
+
+    public ValidationResult deleteMember(String userName, String groupId, String deletedBy) {
+        ValidationResult validationResult = groupService.deleteMembership(userName, groupId, deletedBy);
+        return validationResult;
+    }
+
+    public ValidationResult resignMembership(String userName, String groupId) {
+        ValidationResult validationResult = groupService.resignMembership(userName, groupId);
+        return validationResult;
+    }
+
+    public ValidationResult acceptMembership(String userName, String groupId, String acceptedBy) {
+        ValidationResult validationResult = groupService.acceptMembership(userName, groupId, acceptedBy);
+        return validationResult;
+    }
+
+    public ValidationResult joinGroup(String userName, String groupId) {
+        ValidationResult validationResult;
+        HashMap<String, Group> groups = groupService.getGroups();
+        Group group = groups.get(groupId);
+        if (group.getGroupType().equals(GroupType.PUBLIC)) {
+            validationResult = groupService.assignMembership(userName, groupId, "VIEWER");
+        } else {
+            validationResult = groupService.requestMembership(userName, groupId, "VIEWER");
+        }
+        return validationResult;
+    }
+
+    public ValidationResult rejectMembership(String userName, String groupId) {
+        ValidationResult validationResult = groupService.rejectMembership(userName, groupId);
+        return validationResult;
+    }
+
+    public ValidationResult updateMembership(String userName, String groupId, String updatedBy, String updatedTo) {
+        ValidationResult validationResult = groupService.updateMembership(userName, groupId, updatedBy, updatedTo);
+        return validationResult;
+    }
+
+    public ValidationResult createUser(String userName) {
+        ValidationResult validationResult = groupService.createUser(userName);
+        return validationResult;
     }
 
 
