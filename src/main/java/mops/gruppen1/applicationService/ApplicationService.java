e4335f2c70198c4eb2622e1682e262e149daac0d
TImA97 <tim.angelike@gmx.de>
2020-03-24 10:40:12 +0100

    Add get and search methods to applicationService
    co-authored by: nibrand nils.brandenburg@hhu.de

--- src/main/java/mops/gruppen1/applicationService/ApplicationService.java
+++ src/main/java/mops/gruppen1/applicationService/ApplicationService.java
@@ -1,8 +1,76 @@
 package mops.gruppen1.applicationService;
 
+import mops.gruppen1.domain.Group;
+import mops.gruppen1.domain.Membership;
+import mops.gruppen1.domain.MembershipStatus;
+import mops.gruppen1.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ApplicationService {
     @Autowired
     GroupService groupService;
+
+    public List<Membership> getMembersOfGroup(String groupId) {
+        HashMap<String, List<Membership>> groupToMembers = groupService.getGroupToMembers();
+        List<Membership> memberships = groupToMembers.get(groupId);
+        return memberships;
+    }
+
+    public List<Group> getGroupsOfUser(String userName) {
+        HashMap<String, List<Membership>> userToMembers = groupService.getUserToMembers();
+        List<Membership> memberships = userToMembers.get(userName);
+        List<Group> groups = memberships.stream().map(member -> member.getGroup()).collect(Collectors.toList());
+        return groups;
+    }
+
+    public List<Membership> getPendingRequestOfGroup(String groupId) {
+        HashMap<String, List<Membership>> groupToMembers = groupService.getGroupToMembers();
+        List<Membership> memberships = groupToMembers.get(groupId);
+        List<Membership> pendingMemberships = memberships.stream()
+                .filter(membership -> membership.getMembershipStatus().equals(MembershipStatus.PENDING))
+                .collect(Collectors.toList());
+        return pendingMemberships;
+    }
+
+    public long countPendingRequestOfGroup(String groupId) {
+        HashMap<String, List<Membership>> groupToMembers = groupService.getGroupToMembers();
+        List<Membership> memberships = groupToMembers.get(groupId);
+        long pendingMemberships = memberships.stream()
+                .filter(membership -> membership.getMembershipStatus().equals(MembershipStatus.PENDING))
+                .count();
+        return pendingMemberships;
+    }
+
+    public List<Group> searchGroupByName(String groupName) {
+        HashMap<String, Group> groups = groupService.getGroups();
+        List<Group> requestedGroups = new ArrayList<>();
+        for (Group group : groups.values()) {
+            if (group.getName().toString().contains(groupName)) {
+                requestedGroups.add(group);
+            }
+        }
+        return requestedGroups;
+    }
+
+    public List<String> searchUserByName(String userName) {
+        HashMap<String, User> users = groupService.getUsers();
+        List<String> requestedUsers = new ArrayList<>();
+        for (User user : users.values()) {
+            if (user.getUsername().toString().contains(userName)) {
+                requestedUsers.add(user.getUsername().toString());
+            }
+        }
+        return requestedUsers;
+    }
+
+    // alle Veranstaltungen als Liste (noch nicht möglich)
+
+    // alle Events
+    
+    // neuer Check für isNotTheOnlyAdmin oder doesGroupStillHaveAnAdmin
 }
