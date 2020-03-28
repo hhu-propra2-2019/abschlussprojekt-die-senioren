7862607e8e3974c7e7808b2d2e42273c37691fff
nibrand <nils.brandenburg@hhu.de>
2020-03-28 14:07:08 +0100

    Fix checkstyle errors in MembershipRequestEvent

--- src/main/java/mops/gruppen1/domain/events/MembershipRequestEvent.java
+++ src/main/java/mops/gruppen1/domain/events/MembershipRequestEvent.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 /**
  * For restricted Groups:
- * Create a Membership with Status 'PENDING'
+ * Create a Membership with Status 'PENDING'.
  * Add Membership to groupToMembers, userToMembers and group's list of members
  */
 @AllArgsConstructor
@@ -27,15 +27,13 @@ public class MembershipRequestEvent implements IEvent {
     private String membershipRequestMessage;
 
     @Override
-    public void execute(HashMap<String, List<Membership>> groupToMembers, HashMap<String, List<Membership>> userToMembers, HashMap<String, User> users, HashMap<String, Group> groups) {
-
-        // TODO: move check to Group Service ?
-        // what about group existence in group, needs to be checked?
+    public void execute(HashMap<String, List<Membership>> groupToMembers, HashMap<String,
+            List<Membership>> userToMembers, HashMap<String, User> users, HashMap<String, Group> groups) {
         if (!users.containsKey(userName)) {
             User newUser = new User(new Username(userName));
             users.put(userName, newUser);
         }
-        // TODO: move check to Group Service ?
+
         if (!userToMembers.containsKey(userName)) {
             userToMembers.put(userName, new ArrayList<>());
         }
@@ -43,7 +41,8 @@ public class MembershipRequestEvent implements IEvent {
         User user = users.get(userName);
         MembershipType membershipType = MembershipType.valueOf(this.membershipType);
         MembershipRequestMessage membershipRequestMessage = new MembershipRequestMessage(this.membershipRequestMessage);
-        Membership membership = new Membership(user, group, membershipType, MembershipStatus.PENDING, membershipRequestMessage);
+        Membership membership = new Membership(user, group, membershipType,
+                MembershipStatus.PENDING, membershipRequestMessage);
 
         group.addMember(membership);
         groupToMembers.get(groupId).add(membership);
