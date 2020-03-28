0f2b5c2fd489a8773810e24360d608d9268e5e3d
TImA97 <tim.angelike@gmx.de>
2020-03-28 13:14:47 +0100

    Change CheckService according to checkStyle warnings

--- src/main/java/mops/gruppen1/applicationService/CheckService.java
+++ src/main/java/mops/gruppen1/applicationService/CheckService.java
@@ -82,8 +82,10 @@ public class CheckService {
         return validationResult;
     }
 
-    public ValidationResult isMembershipActive(String userName, String groupId, HashMap<String, Group> groups,
-                                               HashMap<String, User> users, HashMap<String, List<Membership>> userToMembers) {
+    public ValidationResult isMembershipActive(String userName, String groupId,
+                                               HashMap<String, Group> groups,
+                                               HashMap<String, User> users,
+                                               HashMap<String, List<Membership>> userToMembers) {
         ValidationResult validationResult = new ValidationResult();
         List<Membership> memberships = userToMembers.get(userName);
         Group group = groups.get(groupId);
@@ -101,8 +103,10 @@ public class CheckService {
         return validationResult;
     }
 
-    public ValidationResult isMembershipPending(String userName, String groupId, HashMap<String, Group> groups,
-                                                HashMap<String, User> users, HashMap<String, List<Membership>> userToMembers) {
+    public ValidationResult isMembershipPending(String userName, String groupId,
+                                                HashMap<String, Group> groups,
+                                                HashMap<String, User> users,
+                                                HashMap<String, List<Membership>> userToMembers) {
         ValidationResult validationResult = new ValidationResult();
         List<Membership> memberships = userToMembers.get(userName);
         Group group = groups.get(groupId);
@@ -153,11 +157,13 @@ public class CheckService {
                                                HashMap<String, List<Membership>> groupToMembers) {
         ValidationResult validationResult = new ValidationResult();
         List<Membership> memberships = groupToMembers.get(groupId);
-        Membership membership = memberships.stream().filter(m -> m.getUser().getUsername().toString().equals(modifiedBy)).findFirst().orElse(null);
+        Membership membership = memberships.stream()
+                .filter(m -> m.getUser().getUsername().toString().equals(modifiedBy)).findFirst().orElse(null);
         boolean isAdmin = membership.getMembershipType().equals(MembershipType.ADMIN);
         if (isAdmin && modifiedBy.equals(modifiedUser)) {
 
-            long adminCount = memberships.stream().filter(m -> m.getMembershipType().equals(MembershipType.ADMIN)).count();
+            long adminCount = memberships.stream().
+                    filter(m -> m.getMembershipType().equals(MembershipType.ADMIN)).count();
             if (adminCount < 2) {
                 validationResult.addError("Es muss zuerst ein anderer Administrator bestimmt werden.");
             }
@@ -168,7 +174,9 @@ public class CheckService {
     private Membership getMembership(List<Membership> memberships, Group group) {
         Membership membership = null;
 
-        if(memberships == null) return null;
+        if(memberships == null){
+            return null;
+        }
 
         for (Membership m : memberships) {
             if (m.getGroup().equals(group)) {
